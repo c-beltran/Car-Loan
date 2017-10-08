6ccd8002e2332b147a5340276534d89e44f94935
c-beltran <carlosbeltran90@gmail.com>
2017-10-08 00:30:23 -0400

    using grids

---
+++ 2D-Game/src/dev/carlos/tilegame/gfx/Assets.java
@@ -0,0 +1,15 @@
+package dev.carlos.tilegame.gfx;
+
+import java.awt.image.BufferedImage;
+
+/*
+ * this class contains any music, image, sound
+ */
+public class Assets {
+
+	public static BufferedImage player, dirt, grass, stone, tree;
+	
+	public static void init(){
+		
+	}
+}
